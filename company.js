b75ed06f97f11706eecc0e5c5143a7fd70cc7f7a
Mone't Fulgham <monetfulgham@gmail.com>
2018-03-31 00:05:49 -0400

    Add divs to DOM according to number of companies in companyList.

--- company.js
+++ company.js
@@ -1,4 +1,5 @@
-let companyList = [{
+let companyList = [
+    {
         "Company Name": "ABC REIT",
         "Status": "Researching",
         "Company Information": "ABC REIT specializes in commerical real estate properties. Their specialties include property sales, leasing, leins, and land.",
@@ -23,40 +24,69 @@ let companyList = [{
     }
 
 ]
+let name = "123 Construction Company, LLC";
+let status = "Pending";
+let companyInfo= "Construction Company";
+let keyContacts = "Mone\'t Fulgham; Phone: 4049019199, Email: monetfulgham@gmail.com";
+let finPerf = "On track: Scheduled to develop 10 properties in Q2 2018";
 
-function addDiv(){
-    console.log("Loaded");
-    let newDiv=`<section class="newCompany col-6"><h2>The company name</h2></section>`;
-    $('future-investment').append(newDiv);
-    console.log(newDiv, 'done');
+function createNewDiv(index){
+    let newDiv=`<section class="newCompany"` + index + `col-6"></section>`;
+    $('#future-investment').append(newDiv);
 }
 
+function createMultipleDivs(arrayOfObjs){
+    arrayOfObjs.forEach(function(index){
+        console.log("Executed", index);
+        createNewDiv(index);
+    });
+}
+
+//so far I've added multiple divs to the DOM for each company in companyList. Next is to figure out the loop to add Company
+//Names correctly to each div. Will stop here tonight and pick up tomorrow.
 
 function addCompanyName(name) {
     //add the company name to the div
-    let section = document.getElementById('newCompany');
-    name = "Company 123";
-    section.innerHTML= name;
+    $('.newCompany').append("<h2>" + name + "</h2>");
+}
+function addObjectCompanyName(objectOfCompany){
+    let first= objectOfCompany[0]["Company Name"];
+    addCompanyName(first);
 }
 
-
-// var d1 = document.getElementById('one');
-// d1.insertAdjacentHTML('beforeend', '<div id="two">two</div>');
-
-function addAllCompanyNames() {
-    //add ALL company names with h3 tag
+function addMultipleCompanyNames(arrayOfObjs){
+    arrayOfObjs.forEach(function (names){
+        addObjectCompanyName(names);
+    })
 }
 
-function addCompanyStatus() {
-
+function addCompanyStatus(status) {
+    $('.newCompany').append("<li><b>Status</b>: " + status + "</li>");
 }
 
-function addAllCompanyStatuses() {
-
+function addCompanyInfo(companyInfo){
+    $('.newCompany').append("<li><b>Company Info</b>: " + companyInfo + "</li>");
 }
 
-function addAllSections() {
-    //add sections for each company
+function addKeyContacts(keyContacts){
+    $('.newCompany').append("<li><b>Key Contacts</b>: " + keyContacts + "</li>");
 }
 
+function addFinancialPerformance(finPerf){
+    $('.newCompany').append("<li><b>Financial Performance</b>: " + finPerf + "</li></ul>");
+}
+
+function addButton(){
+    $('.newCompany').append("<button>Edit</button><button>Delete</button>");
+}
+
+function addDiv(){
+    createMultipleDivs(companyList);
+    addMultipleCompanyNames(companyList);
+    addCompanyStatus(status);
+    addCompanyInfo(companyInfo);
+    addKeyContacts(keyContacts);
+    addFinancialPerformance(finPerf);
+    addButton();
+}
 $(addDiv)
